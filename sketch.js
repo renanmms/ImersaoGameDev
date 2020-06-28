c374af0984d4e792a3547d89d194071656a637e9
Renan Martins <renanmartins1999@hotmail.com>
2020-06-28 18:05:14 -0300

    First commit

--- sketch.js
+++ sketch.js
@@ -1,21 +1,9 @@
 // JavaScript source code
-let cenarioImagem;
-let imagemPersonagem;
-//Carrega a florestinha
-function setup(){
-	createCanvas(windowWidth, windowHeight);
-	console.log('setup');
-}
+//Variaveis para guardar as imagens
 
-//Mostra a florestinha
 
-function draw(){
-	background(200);
-	circle(mouseX, mouseY, 200);
-//	console.log('draw');
-}
 
-function preload(){
-	cenarioImagem = loadImage("images/cenario/florest.jpg");
-}
+
+
+
 
